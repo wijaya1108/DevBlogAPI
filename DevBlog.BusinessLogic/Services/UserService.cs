89fd6c687af5280cb12043d717dce96788cd3793
uditha bandara <wijaya.uditha98@gmail.com>
2025-02-16 22:51:22 +0530

    add user post endpoint, scalar, user repo and user BLogics and DTOs.

---
+++ DevBlog.BusinessLogic/Services/UserService.cs
@@ -0,0 +1,41 @@
+﻿using DevBlog.BusinessLogic.DTO.Requests;
+using DevBlog.BusinessLogic.Interfaces;
+using DevBlog.Data.Models;
+using DevBlog.Data.Repositories.Users;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevBlog.BusinessLogic.Services
+{
+    public class UserService : IUserService
+    {
+        private readonly IUserRepository _userRepository;
+
+        public UserService(IUserRepository userRepository)
+        {
+            _userRepository = userRepository;
+        }
+
+        public async Task<bool> CreateUser(UserCreateRequest request)
+        {
+            //TODO - check if user is already there by email/name
+            //TODO - hash the password
+
+            var user = new User()
+            {
+                FirstName = request.FirstName,
+                LastName = request.LastName,
+                Email = request.Email,
+                Password = request.Password,
+                IsDeleted = false
+            };
+
+            var result = await _userRepository.CreateUser(user);
+
+            return result;
+        }
+    }
+}
---
+++ DevBlog.Data/Repositories/Users/UserRepository.cs
@@ -0,0 +1,37 @@
+﻿using DevBlog.Data.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevBlog.Data.Repositories.Users
+{
+    public class UserRepository : IUserRepository
+    {
+        private readonly DevBlogDbContext _dbContext;
+        private readonly ILogger<UserRepository> _logger;
+
+        public UserRepository(DevBlogDbContext dbContext, ILogger<UserRepository> logger)
+        {
+            _dbContext = dbContext;
+            _logger = logger;
+        }
+        public async Task<bool> CreateUser(User user)
+        {
+            try
+            {
+                await _dbContext.Users.AddAsync(user);
+                await _dbContext.SaveChangesAsync();
+                return true;
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError("User create operation failed: {0}", ex.Message);
+                return false;
+            }
+        }
+    }
+}
