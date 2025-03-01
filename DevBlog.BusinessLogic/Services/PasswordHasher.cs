0b6db343a2fbe24f796cfe4d76fae942019ea8c6
uditha bandara <wijaya.uditha98@gmail.com>
2025-03-01 20:56:25 +0530

    add password hashing service

---
+++ DevBlog.BusinessLogic/Services/PasswordHasher.cs
@@ -0,0 +1,29 @@
+﻿using DevBlog.BusinessLogic.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevBlog.BusinessLogic.Services
+{
+    public sealed class PasswordHasher : IPasswordHasher
+    {
+        private const int SaltSize = 16;
+        private const int HashSize = 32;
+        private const int Iterations = 100000;
+
+        private static readonly HashAlgorithmName Algorithm = HashAlgorithmName.SHA512;
+
+        public string Hash(string password)
+        {
+            byte[] salt = RandomNumberGenerator.GetBytes(SaltSize);
+            byte[] hash = Rfc2898DeriveBytes.Pbkdf2(password, salt, Iterations, Algorithm, HashSize);
+
+            var hashedPassword = $"{Convert.ToHexString(hash)} - {Convert.ToHexString(salt)}";
+
+            return hashedPassword;
+        }
+    }
+}
