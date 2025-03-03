8512d5df6c9460edd7df3c5da71230febd6f6ab4
uditha bandara <wijaya.uditha98@gmail.com>
2025-03-03 19:39:09 +0530

    halfly completed login logic

---
+++ DevBlog.BusinessLogic/DTO/Responses/LoginResponse.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevBlog.BusinessLogic.DTO.Responses
+{
+    public class LoginResponse
+    {
+        public bool Success { get; set; }
+        public string? Message { get; set; }
+        public string? Email { get; set; }
+    }
+}
---
+++ DevBlog.BusinessLogic/Interfaces/IAuthService.cs
@@ -0,0 +1,15 @@
+﻿using DevBlog.BusinessLogic.DTO.Requests;
+using DevBlog.BusinessLogic.DTO.Responses;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevBlog.BusinessLogic.Interfaces
+{
+    public interface IAuthService
+    {
+        Task<LoginResponse> LoginUser(LoginRequest request);
+    }
+}
