2838e7dfe06e80af58828deaeeeec44dc29cabc4
uditha bandara <wijaya.uditha98@gmail.com>
2025-03-08 10:12:03 +0530

    implement login method

--- DevBlog/Endpoints/AuthModule.cs
+++ DevBlog/Endpoints/AuthModule.cs
@@ -1,5 +1,7 @@
 ﻿using DevBlog.BusinessLogic.DTO.Requests;
+using DevBlog.BusinessLogic.DTO.Responses;
 using DevBlog.BusinessLogic.Interfaces;
+using DevBlog.Validators;
 
 namespace DevBlog.Endpoints
 {
@@ -9,10 +11,24 @@ namespace DevBlog.Endpoints
         {
             app.MapPost("/login", async (IAuthService _authService, LoginRequest request) =>
             {
-                //TODO - validation
+                var validator = new LoginRequestValidator();
+                var validationResult = await validator.ValidateAsync(request);
+
+                if (!validationResult.IsValid)
+                {
+                    var errors = validationResult.Errors.Select(e => e.ErrorMessage).ToList();
+                    LoginResponse loginResponse = new()
+                    {
+                        Success = false,
+                        Errors = errors
+                    };
+
+                    return Results.BadRequest(loginResponse);
+                }
+
                 var result = await _authService.LoginUser(request);
 
-                return result;
+                return Results.Ok(result);
             });
         }
     }
---
+++ DevBlog/Validators/LoginRequestValidator.cs
@@ -0,0 +1,19 @@
+﻿using DevBlog.BusinessLogic.DTO.Requests;
+using FluentValidation;
+
+namespace DevBlog.Validators
+{
+    public class LoginRequestValidator : AbstractValidator<LoginRequest>
+    {
+        public LoginRequestValidator()
+        {
+            RuleFor(m => m.Email)
+                .NotEmpty()
+                .WithMessage("Email is required");
+
+            RuleFor(m => m.Password)
+                .NotEmpty()
+                .WithMessage("Password is required");
+        }
+    }
+}
